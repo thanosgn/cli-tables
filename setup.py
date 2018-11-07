16a499ace34b14a2347f415624eb85c7e3b64080
Thanos Giannopoulos <thanosgn@gmail.com>
2018-11-07 15:33:02 +0200

    Edit layout for pypi packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ascii-tables",
+    version="0.0.3",
+    author="Thanos Giannooulos",
+    author_email="thanosgn@gmail.com",
+    description="Create pretty looking ascii tables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thanosgn/ascii-tables",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
